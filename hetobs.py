efab921ffd48e64dff9439ee09c20156dda07c4e
grzeimann <grzeimann@gmail.com>
2017-12-05 08:01:43 -0600

    initial commit of hetobs.py as a skelton

---
+++ hetobs.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 07:30:47 2017
+
+@author: gregz
+"""
+
+rootdir = ''
+observation = ''
+
+
+def get_acam_list_by_obs(lag=5):
+    pass
+
+
+def get_guider_list_by_obs(lag=5):
+    pass
+
+
+def reduce_acam(image):
+    pass
+
+
+def reduce_guider(kind):
+    pass
+
+
+def get_stars_for_obs(ra, dec):
+    pass
+
+
+def get_stars_for_acam(catalog):
+    pass
+
+
+def get_stars_for_probe(catalog, kind):
+    pass
+
+
+def set_initial_acam_astrometry(ra, dec, pa):
+    pass
+
+
+def get_astrometry_acam(init_astrom, stars):
+    pass
+
+
